9599d142514e95f977f09bb744a52bf3c6939ad7
Iman Khalid <imankhalid@Imans-MacBook-Air.local>
2022-06-02 17:49:28 +0200

    week01

--- server.mjs
+++ server.mjs
@@ -1,54 +1,31 @@
 
-// import express from 'express';
-// import serveStatic from 'serve-static';
-// import compression from 'compression';
-
-
-// const app = express();
-// const server = 4000;
-
-// app.use(compression());
-// app.use(
-//   serveStatic('dist', {
-//     maxAge: '1y',
-//     setHeaders: setCustomCacheControl,
-//   })
-// );
-
-// app.listen(server);
-// function setCustomCacheControl(res, path) {
-//   if (serveStatic.mime.lookup(path) === 'text/html') {
-
-//     res.setHeader(
-//       'Cache-Control',
-//       'public, max-age=300,stale-while-revalidate=60'
-//     );
-//   }
-// }
-
-
-
-
-
 import express from 'express';
-import compression from 'compression';
 import serveStatic from 'serve-static';
-const server = express();
-const port = 4000;
-server.use(compression());
-server.use(
+import compression from 'compression';
+
+
+const app = express();
+const server = 4000;
+
+app.use(compression());
+app.use(
   serveStatic('dist', {
-    maxAge: '1d',
+    maxAge: '1y',
     setHeaders: setCustomCacheControl,
   })
 );
-server.listen(port);
+
+app.listen(server);
 function setCustomCacheControl(res, path) {
   if (serveStatic.mime.lookup(path) === 'text/html') {
-    // Custom Cache-Control for HTML files
+
     res.setHeader(
       'Cache-Control',
-      'public, max-age=60,stale-while-revalidate=60'
+      'public, max-age=300,stale-while-revalidate=60'
     );
   }
 }
+
+
+
+
